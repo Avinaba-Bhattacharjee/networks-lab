0012e60c5495a763d6f884bf8b371cbd60f6d0d3
Avinaba Bhattacharjee <avinababhattacharjee2002@gmail.com>
2023-05-24 23:50:56 +0530

    Assignment-6 added

---
+++ assignment_6/TimeClient.java
@@ -0,0 +1,34 @@
+package assignment_6;
+
+import java.net.*;
+import java.util.Date;
+import java.io.*;
+
+public class TimeClient {
+    public static void main(String[] args) throws IOException {
+        Socket timeSocket = null;
+        ObjectInputStream timeIn = null;
+
+        try {
+            timeSocket = new Socket("localhost", 1313);
+            timeIn = new ObjectInputStream(timeSocket.getInputStream());
+        } catch(UnknownHostException e) {
+            System.err.println("Server localhost can't be reached");
+            System.exit(1);
+        } catch(IOException e) {
+            System.err.println("Error during connection to localhost");
+            System.exit(1);
+        }
+        
+        try {
+            Date serverDate = (Date)timeIn.readObject();
+            System.err.println("Current server time: " + serverDate);
+        } catch(ClassNotFoundException e) {
+            System.err.println("Not a Date object returned");
+            System.exit(1);
+        }
+
+        timeIn.close();
+        timeSocket.close();
+    }
+}
---
+++ assignment_6/TimeServer.java
@@ -0,0 +1,36 @@
+package assignment_6;
+
+import java.net.*;
+import java.util.Date;
+import java.io.*;
+
+public class TimeServer {
+    public static void main(String[] args) throws IOException {
+        ServerSocket servSocket = null;
+
+        try {
+            servSocket = new ServerSocket(1313);
+        } catch(IOException e) {
+            System.err.println("Could not listen on port 1313");
+            System.exit(1);
+        }
+
+        Socket clientSocket = null;
+
+        for (;;) {
+            try {
+                clientSocket = servSocket.accept();
+            } catch(IOException e) {
+                System.err.println("Accept failed");
+                System.exit(1);
+            }
+
+            ObjectOutputStream timeOut = new ObjectOutputStream(clientSocket.getOutputStream());
+            timeOut.writeObject(new Date());
+            timeOut.close();
+            clientSocket.close();
+
+            //servSocket.close();
+        }
+    }
+}
